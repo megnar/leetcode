27bea9cca1797db5a358a4e46ab962665630ce1b
Evgeny <poturiloz@mail.ru>
2024-07-29 19:34:29 +0300

    add 124

---
+++ 124.py
@@ -0,0 +1,47 @@
+"""A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+The path sum of a path is the sum of the node's values in the path.
+
+Given the root of a binary tree, return the maximum path sum of any non-empty path."""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        node, target = self.collectAllDataRecursive(root)
+        return target
+    
+    def collectAllDataRecursive(self, root: Optional[TreeNode]) -> tuple:
+        if root is None:
+            return None, None
+        target = root.val # max of node,
+                            #sum_r.node + root.val + sum_l.node,  sum_l.target, sum_r.target
+        node = root.val  # max root.val, sum_l.node + root.val, sum_r.node + root.val
+        node_l, target_l = self.collectAllDataRecursive(root.left)
+        node_r, target_r = self.collectAllDataRecursive(root.right)
+        
+        # compute node value
+        if node_l is not None:
+            node = max(node, node_l + root.val)
+            target = max(target, target_l)
+        if node_r is not None:
+            node = max(node, node_r + root.val)
+            target = max(target, target_r)
+        target = max(target, node)
+        #compute target value
+        if node_l is not None and node_r is not None:
+            target = max(node_l + root.val + node_r, target)
+
+        return node, target
+        
+    
+
+
+
+
+        
---
+++ 228.py
@@ -0,0 +1,34 @@
+"""You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b"""
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+        if len(nums) == 0:
+            return []
+        left_bound = 0
+        prev_elem = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] - prev_elem > 1:
+                if i - left_bound > 1:
+                    res_str = f"{nums[left_bound]}->{nums[i - 1]}"
+                else: 
+                    res_str = f"{nums[left_bound]}"
+                result.append(res_str)
+                left_bound = i
+            prev_elem = nums[i]
+        i = len(nums) - 1
+        if i - left_bound > 0:
+            res_str = f"{nums[left_bound]}->{nums[i]}"
+        else: 
+            res_str = f"{nums[left_bound]}"
+        result.append(res_str)
+        return result
